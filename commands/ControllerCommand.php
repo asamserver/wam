bd0c87c7dc51bd8f835a5611a5d3f71814bb3891
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-24 13:44:36 +0330

    first commmit

--- commands/ControllerCommand.php
+++ commands/ControllerCommand.php
@@ -1,5 +1,4 @@
 <?php
-// commands/ControllerCommand.php
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -14,8 +13,7 @@ class ControllerCommand extends Command
     {
         $this
             ->setDescription('Create a new controller')
-            ->addArgument('name', InputArgument::REQUIRED, 'The name of the controller (e.g., Dashboard)')
-            // No need for 'type' argument, type is inferred from the directory structure
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the controller (e.g., Admin/DashboardController)')
             ;
     }
 
@@ -27,36 +25,56 @@ class ControllerCommand extends Command
         $currentDir = getcwd();
         $controllerDir = $currentDir . '/src/Controllers'; // Default directory for 'admin'
 
+        // Debug print to check the incoming name argument
+        $output->writeln("<info>Received controller name: $name</info>");
+
+        // Split the path into parts (e.g., Admin/DashboardController -> ['Admin', 'DashboardController'])
+        $pathParts = explode('/', $name);
+        
+        // Determine the namespace and directory structure
+        $namespace = 'WHMCS\\Module\\Addon\\' . basename(getcwd()) . '\\src\\Controllers\\' . implode('\\', array_slice($pathParts, 0, -1));
+        $controllerName = end($pathParts); // The last part is the controller name
+        
+        // Controller directory path
+        $controllerDirPath = $controllerDir . '/' . implode('/', array_slice($pathParts, 0, -1));
+        
+        // Debug print for the target controller directory path
+        $output->writeln("<info>Controller directory path: $controllerDirPath</info>");
+
         // Ensure the target controller directory exists (Create nested directories)
-        if (!is_dir($controllerDir)) {
-            mkdir($controllerDir, 0777, true); // Ensure all directories in the path are created
+        if (!is_dir($controllerDirPath)) {
+            mkdir($controllerDirPath, 0777, true); // Create any necessary subdirectories
+            $output->writeln("<info>Created directory: $controllerDirPath</info>");
         }
 
         // Controller filename and path
-        $controllerName = ucfirst($name);
-        $name=explode('/',$controllerName);
-        $name = $name[count($name)-1];
-        $controllerPath = $controllerDir . '/' . $controllerName . '.php';
-        
+        $controllerPath = $controllerDirPath . '/' . $controllerName . '.php';
+
+        // Debug print for the controller path
+        $output->writeln("<info>Controller path: $controllerPath</info>");
+
         // Load and modify the stub
         $stub = file_get_contents(__DIR__ . '/controller.stub');
         $addonName  = basename(getcwd());
 
         // Replace placeholders in the stub
         $stub = str_replace(
-            ['{{addonName}}','{{name}}','{{controllerName}}'],
-            [$addonName,$name,$controllerName],
+            ['{{addonName}}','{{controllerName}}','{{name}}'],
+            [$addonName, $controllerName, $name],
             $stub
         );
 
+        // Replace namespace with dynamic namespace
+        $stub = str_replace('{{namespace}}', $namespace, $stub);
+
         // Write the controller to the appropriate file
         if (file_put_contents($controllerPath, $stub)) {
-            $output->writeln("<info>Created model: $controllerPath</info>");
+            $output->writeln("<info>Created controller: $controllerPath</info>");
         } else {
-            $output->writeln("<error>Failed to create model file. Please check permissions.</error>");
+            $output->writeln("<error>Failed to create controller file. Please check permissions.</error>");
             return Command::FAILURE;
         }
+
         return Command::SUCCESS;
     }
 }
-
