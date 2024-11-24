9b08a9ea3eecbb97a41f9d6886bf1908d3e4c799
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-24 15:15:24 +0330

    first commmit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+// use WHMCS\Module\Addon\kyc\src\Controllers\Admin\DashboardController;
+// use WHMCS\Module\Addon\kyc\src\Controllers\Client\DashboardController as ClientDashboardController;
+
+// return [
+//     // Admin routes
+//     'admin/dashboard' => [
+//         'controller' => DashboardController::class,
+//         'action' => 'index',
+//     ],
+
+//     // Add more admin routes here...
+    
+//     // Client routes
+//     'client/dashboard' => [
+//         'controller' => ClientDashboardController::class,
+//         'action' => 'index',
+//     ],
+
+//     // Add more client routes here...
+// ];
