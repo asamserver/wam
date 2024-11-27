8228da32d8c64f7366641c1eaeeab4d24f1161ba
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-27 12:40:08 +0330

    first commmit

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
-$this->get('/admin/dashboard', 'DashboardController', 'index');
-$this->get('/client/dashboard', 'ClientDashboardController', 'index');
+use WHMCS\Module\Addon\kyc_verification\app\Controllers\Admin\DashboardController;
+use WHMCS\Module\Addon\kyc_verification\app\Controllers\Client\DashboardController as ClientDashboardController;
+
+$this->get('/admin/dashboard', DashboardController::class, 'index');
+$this->get('/client/dashboard', ClientDashboardController::class, 'index');
