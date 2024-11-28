d1840ef2081fb091b986e0f2af521b98a69399c7
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-28 13:00:52 +0330

    first commmit

--- config/app.php
+++ config/app.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'name' => env('APP_MODULE_NAME') ?? 'kyc',
-    'description' => 'Description for kyc',
+    'name' => env('APP_MODULE_NAME') ?? 'kyc_verification',
+    'description' => 'Description for kyc_verification',
     'author' => env('AUTHOR') ?? 'Your Name',
     'version' => env('VERSION') ?? '1.0.0',
     'enabled' => env('APP_ENABLE') ?? true,
+    'fields' => []
 ];
